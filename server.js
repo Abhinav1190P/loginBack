ec03e591759ff8c00bb37e52448a69f6f4fee6d8
Abhinav <pandeysandeep1190@gmail.com>
2021-06-20 17:45:59 +0530

    alrighh

--- server.js
+++ server.js
@@ -46,9 +46,7 @@ else{
     user.generateToken((err,user)=>{
         if(err) return res.status(400).send(err);
         res.cookie('auth',user.token).json({
-            isAuth:true, 
-            id:user._id,
-            email:user.email
+            isAuth:true
         })
     })   
 }
